014696a5fded59dd15d80729b9ce452e9cd18ebd
Tobias Maestrini <tobias.maestrini@gmail.com>
2026-06-13 15:59:04 +0200

    fix(initializing): Enable auto execution for terminal commands in initialization steps

--- W24 - Taming the chance/.demo/file-contents/Setup/playwright.config.ts
+++ W24 - Taming the chance/.demo/file-contents/Setup/playwright.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "@playwright/test";
 import dotenv from "dotenv";
+import path from "path";
 
-dotenv.config();
+dotenv.config({ path: path.resolve(__dirname, ".env") });
 export default defineConfig({
   reporter: [
     ['html', { outputFolder: 'playwright-report', open: 'never' }],
